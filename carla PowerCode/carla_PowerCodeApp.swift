0b37294c81e424e53e8480fc0d4d6ad5260fada1
CarlaLC <320178258@pcpuma.acatlan.unam.mx>
2023-03-29 13:36:52 -0600

    Initial Commit

---
+++ carla PowerCode/carla_PowerCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  carla_PowerCodeApp.swift
+//  carla PowerCode
+//
+//  Created by CEDAM06 on 29/03/23.
+//
+
+import SwiftUI
+
+@main
+struct carla_PowerCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
